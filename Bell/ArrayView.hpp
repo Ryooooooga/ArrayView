baca205bd6e44076b05e20dd5e0b2042791961ca
Ryoga <ryoga_314@yahoo.co.jp>
2016-05-12 22:09:17 +0900

    :new: テストを追加

--- Bell/ArrayView.hpp
+++ Bell/ArrayView.hpp
@@ -32,14 +32,14 @@ namespace Bell {
 		using reverse_iterator			= std::reverse_iterator<const_iterator>;
 		using const_reverse_iterator	= reverse_iterator;
 		using size_type					= std::size_t;
+		using pos_type					= std::size_t;
 		using difference_type			= std::ptrdiff_t;
 
-	public:
 		//	ctor
 		constexpr ArrayView() noexcept
 			: ptr_(nullptr), size_(0) {}
 
-		constexpr ArrayView(const Type* data, std::size_t length) noexcept
+		constexpr ArrayView(const Type* data, size_type length) noexcept
 			: ptr_(data), size_(ptr_ ? length : 0) {}
 
 		template <std::size_t N>
@@ -52,10 +52,10 @@ namespace Bell {
 
 		template <typename Allocator>
 		ArrayView(const std::vector<Type, Allocator>& v) noexcept
-			: ptr_(a.data()), size_(a.size()) {}
+			: ptr_(v.data()), size_(v.size()) {}
 
 		template <typename Traits, typename Allocator>
-		ArrayView(const std::basic_string<Type, Tratis, Allocator>& s) noexcept
+		ArrayView(const std::basic_string<Type, Traits, Allocator>& s) noexcept
 			: ptr_(s.data()), size_(s.size()) {}
 
 		template <typename Traits>
@@ -73,9 +73,9 @@ namespace Bell {
 			return !equals(a);
 		}
 
-		constexpr const Type& operator [](std::size_t pos) const noexcept
+		constexpr const Type& operator [](pos_type pos) const noexcept
 		{
-			return assert(pos < size_), data_[pos];
+			return assert(pos < size_), ptr_[pos];
 		}
 
 		//	range
@@ -101,7 +101,7 @@ namespace Bell {
 
 		const_reverse_iterator rbegin() const noexcept
 		{
-			return reverce_iterator(end());
+			return reverse_iterator(end());
 		}
 
 		const_reverse_iterator crbegin() const noexcept
@@ -132,56 +132,57 @@ namespace Bell {
 
 		constexpr const Type* data() const noexcept
 		{
-			return data_;
+			return ptr_;
 		}
 
 		//	element access
 		constexpr const Type& front() const noexcept
 		{
-			return assert(size_ > 0), data_[0];
+			return assert(size_ > 0), ptr_[0];
 		}
 
 		constexpr const Type& back() const noexcept
 		{
-			return assert(size_ > 0), data_[size_ - 1];
+			return assert(size_ > 0), ptr_[size_ - 1];
 		}
 
-		constexpr const Type& at(std::size_t pos) const
+		constexpr const Type& at(pos_type pos) const
 		{
-			return size_ > 0
-				? throw std::out_of_range("ArrayView::at out_of_range")
-				: data_[pos];
+			return (void)(pos < size_ ? 0 : throw std::out_of_range("ArrayView::at out_of_range")), ptr_[pos];	//	:anger: コンパイラバグ回避
+			//return pos < size_
+			//	? ptr_[pos]
+			//	: throw std::out_of_range("ArrayView::at out_of_range");
 		}
 
 		//	comparison
 		bool equals(const ArrayView& a) const noexcept
 		{
-			return equals(begin(), end(), a.begin(), a.end());
+			return std::equal(begin(), end(), a.begin(), a.end());
 		}
 
 		template <typename Pred>
 		bool equals(const ArrayView& a, Pred&& pred) const noexcept
 		{
-			return equals(begin(), end(), a.begin(), a.end(), std::forward<Pred>(pred));
+			return std::equal(begin(), end(), a.begin(), a.end(), std::forward<Pred>(pred));
 		}
 
 		//	conversion
 		template <typename Allocator = std::allocator<Type>>
-		std::vector<Type, Allocator> toVec() const
+		std::vector<Type, Allocator> toVector() const
 		{
 			return std::vector<Type, Allocator>(begin(), end());
 		}
 
 		template <typename Traits = std::char_traits<Type>, typename Allocator = std::allocator<Type>>
-		std::basic_string<Type, Traits, Allocator> toStr() const
+		std::basic_string<Type, Traits, Allocator> toString() const
 		{
 			return std::basic_string<Type, Traits, Allocator>(ptr_, size_);
 		}
 
 		template <typename Traits = std::char_traits<Type>>
-		constexpr boost::basic_string_ref<Type, Traits> toStrRef() const noexcept
+		constexpr boost::basic_string_ref<Type, Traits> toStringRef() const noexcept
 		{
-			return boost::basic_string_ref<Type, Trais>(ptr_, size_);
+			return boost::basic_string_ref<Type, Traits>(ptr_, size_);
 		}
 
 		ArrayView& clear() noexcept
@@ -192,7 +193,7 @@ namespace Bell {
 			return *this;
 		}
 
-		ArrayView& removePrefix(std::size_t n) noexcept
+		ArrayView& removePrefix(size_type n) noexcept
 		{
 			n = std::min(n, size_);
 			ptr_ += n;
@@ -201,7 +202,7 @@ namespace Bell {
 			return *this;
 		}
 		
-		ArrayView& removeSuffix(std::size_t n) noexcept
+		ArrayView& removeSuffix(size_type n) noexcept
 		{
 			n = std::min(n, size_);
 			size_ -= n;
@@ -217,6 +218,20 @@ namespace Bell {
 		ArrayView& popBack() noexcept
 		{
 			return removeSuffix(1);
+		}
+
+		constexpr ArrayView substring(pos_type pos, size_type len = std::numeric_limits<size_type>::max()) const noexcept
+		{
+			return pos >= size_
+				? ArrayView {}
+				: ArrayView { ptr_ + pos, std::min(size_ - pos, len) };
+		}
+
+		constexpr ArrayView slice(pos_type begin, pos_type end) const noexcept
+		{
+			return end > begin
+				? substring(begin, end - begin)
+				: ArrayView {};
 		}
 	};
 
---
+++ Test/Bell/Test_ArrayView.cpp
@@ -0,0 +1,293 @@
+﻿//=====================================================================
+// Copyright (c) 2015-2016 Ryooooooga.
+// https://github.com/Ryooooooga
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=====================================================================
+
+#include <gtest/gtest.h>
+#include "../Bell/ArrayView.hpp"
+
+using namespace std;
+using namespace boost;
+using namespace Bell;
+
+namespace
+{
+	static_assert(is_same<ArrayView<int>::value_type			, int							>::value, "");
+	static_assert(is_same<ArrayView<int>::pointer				, int*							>::value, "");
+	static_assert(is_same<ArrayView<int>::const_pointer			, const int*					>::value, "");
+	static_assert(is_same<ArrayView<int>::reference				, int&							>::value, "");
+	static_assert(is_same<ArrayView<int>::const_reference		, const int&					>::value, "");
+	static_assert(is_same<ArrayView<int>::iterator				, const int*					>::value, "");
+	static_assert(is_same<ArrayView<int>::const_iterator		, const int*					>::value, "");
+	static_assert(is_same<ArrayView<int>::reverse_iterator		, reverse_iterator<const int*>	>::value, "");
+	static_assert(is_same<ArrayView<int>::const_reverse_iterator, reverse_iterator<const int*>	>::value, "");
+	static_assert(is_same<ArrayView<int>::size_type				, size_t						>::value, "");
+	static_assert(is_same<ArrayView<int>::pos_type				, size_t						>::value, "");
+	static_assert(is_same<ArrayView<int>::difference_type		, ptrdiff_t						>::value, "");
+}
+
+TEST(Bell_ArrayView, Constructor_ConstExpr)
+{
+	constexpr int a[] = { 0, 1, 2, 3 };
+
+	constexpr ArrayView<int> x1;
+	constexpr ArrayView<int> x2 = a;
+	constexpr ArrayView<int> x3 = ArrayView<int>(a, 2);
+	constexpr ArrayView<int> x4 = ArrayView<int>(nullptr, 4);
+	constexpr ArrayView<int> x5 = x2;
+
+	static_assert(x1.data() == nullptr, "");
+	static_assert(x1.size() == 0, "");
+
+	static_assert(x2.data() == a, "");
+	static_assert(x2.size() == 4, "");
+
+	static_assert(x3.data() == a, "");
+	static_assert(x3.size() == 2, "");
+
+	static_assert(x4.data() == nullptr, "");
+	static_assert(x4.size() == 0, "");
+
+	static_assert(x5.data() == x2.data(), "");
+	static_assert(x5.size() == x2.size(), "");
+}
+
+TEST(Bell_ArrayView, Constructor)
+{
+	const array<int, 4>	a = {{ 0, 1, 2, 3 }};
+	const vector<int>	b = { 0, 1, 2, 3 };
+	const string		c = u8"Test";
+	const string_ref	d = c;
+
+	const ArrayView<int>  x0 = a;
+	const ArrayView<int>  x1 = b;
+	const ArrayView<char> x2 = c;
+	const ArrayView<char> x3 = d;
+
+	EXPECT_EQ(a.data(), x0.data());
+	EXPECT_EQ(a.size(), x0.size());
+
+	EXPECT_EQ(b.data(), x1.data());
+	EXPECT_EQ(b.size(), x1.size());
+
+	EXPECT_EQ(c.data(), x2.data());
+	EXPECT_EQ(c.size(), x2.size());
+
+	EXPECT_EQ(d.data(), x3.data());
+	EXPECT_EQ(d.size(), x3.size());
+}
+
+TEST(Bell_ArrayView, Operators)
+{
+	constexpr int a[] = { 0, 1, 2, 3 };
+	constexpr int b[] = { 0, 1, 2 };
+
+	constexpr ArrayView<int> x0 = a;
+	constexpr ArrayView<int> x1 = b;
+	constexpr ArrayView<int> x2 = ArrayView<int>(a, 3);
+
+	//	==
+	EXPECT_TRUE (x0 == x0);
+	EXPECT_FALSE(x0 == x1);
+	EXPECT_FALSE(x0 == x2);
+	EXPECT_TRUE (x1 == x1);
+	EXPECT_TRUE (x1 == x2);
+	EXPECT_TRUE (x2 == x2);
+
+	//	!=
+	EXPECT_FALSE(x0 != x0);
+	EXPECT_TRUE (x0 != x1);
+	EXPECT_TRUE (x0 != x2);
+	EXPECT_FALSE(x1 != x1);
+	EXPECT_FALSE(x1 != x2);
+	EXPECT_FALSE(x2 != x2);
+
+	//	[]
+	static_assert(x0[0] == 0, "");
+	static_assert(x0[1] == 1, "");
+	static_assert(x0[2] == 2, "");
+	static_assert(x0[3] == 3, "");
+	//static_assert(x0[4], "");
+
+	static_assert(x1[0] == 0, "");
+	static_assert(x1[1] == 1, "");
+	static_assert(x1[2] == 2, "");
+	//static_assert(x1[3], "");
+}
+
+TEST(Bell_ArrayView, Range)
+{
+	constexpr int a[] = { 0, 1, 2, 3 };
+	constexpr ArrayView<int> x0 = a;
+
+	static_assert(x0.begin	() == x0.data()				, "");
+	static_assert(x0.end	() == x0.data() + x0.size()	, "");
+	static_assert(x0.cbegin	() == x0.data()				, "");
+	static_assert(x0.cend	() == x0.data() + x0.size()	, "");
+
+	EXPECT_EQ(reverse_iterator<const int*>(x0.end()), x0.rbegin());
+	EXPECT_EQ(reverse_iterator<const int*>(x0.end()), x0.crbegin());
+	EXPECT_EQ(reverse_iterator<const int*>(x0.begin()), x0.rend());
+	EXPECT_EQ(reverse_iterator<const int*>(x0.begin()), x0.crend());
+}
+
+TEST(Bell_ArrayView, Container)
+{
+	constexpr int a[] = { 0, 1, 2, 3 };
+
+	constexpr ArrayView<int> x0 = a;
+	constexpr ArrayView<int> x1;
+
+	static_assert(x0.empty() == false, "");
+	static_assert(x1.empty() == true , "");
+}
+
+TEST(Bell_ArrayView, Access)
+{
+	constexpr int a[] = { 0, 1, 2, 3 };
+
+	constexpr ArrayView<int> x0 = a;
+	constexpr ArrayView<int> x1;
+
+	static_assert(x0.front() == 0, "");
+	//static_assert(x1.front(), "");
+
+	static_assert(x0.back() == 3, "");
+	//static_assert(x1.back(), "");
+
+	static_assert(x0.at(0) == 0, "");
+	static_assert(x0.at(1) == 1, "");
+	static_assert(x0.at(2) == 2, "");
+	static_assert(x0.at(3) == 3, "");
+	//static_assert(x0.at(4), "");
+
+	EXPECT_THROW(x0.at(4), std::out_of_range);
+}
+
+TEST(Bell_ArrayView, Equals)
+{
+	constexpr int a[] = { 0, 1, 2, 3 };
+	constexpr int b[] = { 0, 1, 2, 3 };
+	constexpr int c[] = { -2, 0, 1, -2 };
+
+	constexpr ArrayView<int> x0 = a;
+	constexpr ArrayView<int> x1 = b;
+	constexpr ArrayView<int> x2 = c;
+
+	EXPECT_TRUE (x0.equals(x1));
+	EXPECT_FALSE(x0.equals(x2));
+	EXPECT_FALSE(x0.equals(x1, [](int a, int b) { return a > b; }));
+	EXPECT_TRUE (x0.equals(x2, [](int a, int b) { return a > b; }));
+}
+
+TEST(Bell_ArrayView, Conversion)
+{
+	const int			a[]	= { 0, 1, 2, 3 };
+	const vector<int>	b	= { 0, 1, 2, 3 };
+	const string		c	= u8"test";
+
+	const ArrayView<int>	x0 = a;
+	const ArrayView<char>	x1 = c;
+
+	static_assert(is_same<decltype(x0.toVector()	), vector<int>	>::value, "");
+	static_assert(is_same<decltype(x1.toString()	), string		>::value, "");
+	static_assert(is_same<decltype(x1.toStringRef()	), string_ref	>::value, "");
+
+	EXPECT_EQ(b, x0.toVector());
+	EXPECT_EQ(u8"test", x1.toString());
+	EXPECT_EQ(u8"test", x1.toStringRef());
+}
+
+TEST(Bell_ArrayView, Clear)
+{
+	const int a[] = { 0, 1, 2, 3 };
+	ArrayView<int> x = a;
+
+	x.clear();
+
+	EXPECT_EQ(nullptr, x.data());
+	EXPECT_EQ(0, x.size());
+}
+
+TEST(Bell_ArrayView, RemovePrefix)
+{
+	const int a[] = { 0, 1, 2, 3 };
+	ArrayView<int> x = a;
+
+	x.removePrefix(2);
+
+	EXPECT_EQ(a+2, x.data());
+	EXPECT_EQ(2, x.size());
+}
+
+TEST(Bell_ArrayView, RemoveSuffix)
+{
+	const int a[] = { 0, 1, 2, 3 };
+	ArrayView<int> x = a;
+
+	x.removeSuffix(2);
+
+	EXPECT_EQ(a, x.data());
+	EXPECT_EQ(2, x.size());
+}
+
+TEST(Bell_ArrayView, PopFront)
+{
+	const int a[] = { 0, 1, 2, 3 };
+	ArrayView<int> x = a;
+
+	x.popFront();
+
+	EXPECT_EQ(a+1, x.data());
+	EXPECT_EQ(3, x.size());
+}
+
+TEST(Bell_ArrayView, PopBack)
+{
+	const int a[] = { 0, 1, 2, 3 };
+	ArrayView<int> x = a;
+
+	x.popBack();
+
+	EXPECT_EQ(a, x.data());
+	EXPECT_EQ(3, x.size());
+}
+
+TEST(Bell_ArrayView, Substring)
+{
+	constexpr int				a[]	= { 0, 1, 2, 3 };
+	constexpr ArrayView<int>	x0	= a;
+	constexpr ArrayView<int>	x1	= x0.substring(1, 2);
+	constexpr ArrayView<int>	x2	= x0.substring(5, 2);
+	constexpr ArrayView<int>	x3	= x0.substring(1, 8);
+
+	EXPECT_EQ(a+1, x1.data());
+	static_assert(x1.size() == 2, "");
+
+	static_assert(x2.empty(), "");
+
+	EXPECT_EQ(a+1, x3.data());
+	static_assert(x3.size() == 3, "");
+}
+
+TEST(Bell_ArrayView, Slice)
+{
+	constexpr int				a[]	= { 0, 1, 2, 3 };
+	constexpr ArrayView<int>	x0	= a;
+	constexpr ArrayView<int>	x1	= x0.slice(1, 3);
+	constexpr ArrayView<int>	x2	= x0.slice(5, 2);
+	constexpr ArrayView<int>	x3	= x0.slice(1, 8);
+
+	EXPECT_EQ(a+1, x1.data());
+	static_assert(x1.size() == 2, "");
+
+	static_assert(x2.empty(), "");
+
+	EXPECT_EQ(a+1, x3.data());
+	static_assert(x3.size() == 3, "");
+}
+
